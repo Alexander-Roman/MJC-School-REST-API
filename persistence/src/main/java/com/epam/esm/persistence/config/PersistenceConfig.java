222967630be4931b4f99efcb893b2994dd926820
Alexander-Roman <alexander.kitanin@gmail.com>
2021-04-12 23:35:53 +0300

    Profiles implementation

--- persistence/src/main/java/com/epam/esm/persistence/config/PersistenceConfig.java
+++ persistence/src/main/java/com/epam/esm/persistence/config/PersistenceConfig.java
@@ -2,63 +2,42 @@ package com.epam.esm.persistence.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = "com.epam.esm.persistence")
 @EnableTransactionManagement
 public class PersistenceConfig {
 
-    private static final String DEFAULT_HIKARI_PROPS = "property/hikari.properties";
-    private static final String PROFILE_HIKARI_PROPS = "property/hikari-%s.properties";
+    private final String hikariPropertyFileName;
 
-    private final Environment environment;
-
-    @Autowired
-    public PersistenceConfig(Environment environment) {
-        this.environment = environment;
+    public PersistenceConfig(@Value("/property/hikari-${spring.profiles.active:default}.properties") String hikariPropertyFileName) {
+        this.hikariPropertyFileName = hikariPropertyFileName;
     }
 
     @Bean(destroyMethod = "close")
-    public HikariDataSource dataSource() throws IOException {
-        String[] profiles = environment.getActiveProfiles();
-        Optional<String> activeProfile = Arrays.stream(profiles).findFirst();
-        String path = DEFAULT_HIKARI_PROPS;
-        if (activeProfile.isPresent()) {
-            String name = activeProfile.get();
-            path = String.format(PROFILE_HIKARI_PROPS, name);
-        }
-        Resource resource = new ClassPathResource(path);
-        Properties hikariProperties = PropertiesLoaderUtils.loadProperties(resource);
-        HikariConfig hikariConfig = new HikariConfig(hikariProperties);
+    public HikariDataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig(hikariPropertyFileName);
         return new HikariDataSource(hikariConfig);
     }
 
     @Bean
-    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() throws IOException {
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
         DataSource dataSource = dataSource();
         return new NamedParameterJdbcTemplate(dataSource);
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager() throws IOException {
+    public PlatformTransactionManager transactionManager() {
         DataSource dataSource = dataSource();
         return new DataSourceTransactionManager(dataSource);
     }
--- web/src/main/java/com/epam/esm/web/init/WebInitializer.java
+++ web/src/main/java/com/epam/esm/web/init/WebInitializer.java
@@ -3,36 +3,15 @@ package com.epam.esm.web.init;
 import com.epam.esm.persistence.config.PersistenceConfig;
 import com.epam.esm.service.config.ServiceConfig;
 import com.epam.esm.web.config.WebConfig;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.lang.NonNull;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
-import javax.servlet.ServletContext;
-import java.io.IOException;
-import java.util.Properties;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-    private static final String APPLICATION_PROPS = "property/application.properties";
-
-    private final Properties properties;
-
-    public WebInitializer() throws IOException {
-        Resource resource = new ClassPathResource(APPLICATION_PROPS);
-        properties = PropertiesLoaderUtils.loadProperties(resource);
-    }
-
-    @Override
-    protected void registerDispatcherServlet(ServletContext servletContext) {
-        String profile = (String) properties.get("spring.profiles.active");
-        servletContext.setInitParameter("spring.profiles.active", profile);
-        super.registerDispatcherServlet(servletContext);
-    }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
